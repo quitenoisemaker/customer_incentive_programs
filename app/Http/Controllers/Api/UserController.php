fce5fdc36c7260fa0c802b32b3ba8eeb35580798
quitenoisemaker <samsonojugo@gmail.com>
2024-05-19 22:39:15 +0100

    initial installation

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        return 'test';
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+}
